d9765f38e1cc8c4ecd4db880b2b3d60c18a6c800
Adam-Vandervorst <adam.vandervorst@gmail.com>
2022-08-01 02:25:33 +0200

    Rename and add kronecker product

---
+++ collection/src/main/scala/Matrix.scala
@@ -0,0 +1,89 @@
+package macroloop.collection
+
+import scala.reflect.ClassTag
+import compiletime.constValue
+import compiletime.ops.int.*
+
+
+class Matrix[M <: Int, N <: Int, A : ClassTag](val data: Array[A]):
+  inline def nrows: M = constValue
+  inline def ncolumns: N = constValue
+
+  inline def apply(inline i: Int, inline j: Int): A = data(i*ncolumns + j)
+  inline def update(inline i: Int, inline j: Int, inline v: A): Unit = data(i*ncolumns + j) = v
+
+  inline def rows: Iterator[Array[A]] = data.grouped(ncolumns)
+
+  inline def slice[I1 <: Int, I2 <: Int, J1 <: Int, J2 <: Int]: Matrix[I2 - I1, J2 - J1, A] =
+    Matrix.tabulate((i, j) => this(constValue[I1] + i, constValue[J1] + j))
+
+  inline def map[B : ClassTag](inline f: A => B): Matrix[M, N, B] = Matrix(data.map(f))
+  inline def flatMap[O <: Int, P <: Int, B : ClassTag](inline f: A => Matrix[O, P, B]): Matrix[M*O, N*P, B] = map(f).flatten
+
+  inline def convolve[O <: Int, P <: Int, B, C : ClassTag](kernel: Matrix[O, P, B],
+      inline combine: (A, B) => C, inline add: (C, C) => C, inline zero: C): Matrix[M, N, C] =
+    val cx = kernel.nrows/2
+    val cy = kernel.ncolumns/2
+
+    val result = Matrix.fill[M, N, C](zero)
+
+    for i <- 0 until nrows
+        j <- 0 until ncolumns
+        m <- 0 until kernel.nrows
+        n <- 0 until kernel.ncolumns do
+      val ii = i + (m - cx)
+      val jj = j + (n - cy)
+
+      if ii >= 0 && ii < nrows && jj >= 0 && jj < ncolumns then
+        result(i, j) = add(result(i, j), combine(this(ii, jj), kernel(m, n)))
+    result
+
+  inline def multiply[O <: Int, B, C : ClassTag](that: Matrix[N, O, B],
+      inline combine: (A, B) => C, inline add: (C, C) => C, inline zero: C): Matrix[M, O, C] =
+    val result = Matrix.fill[M, O, C](zero)
+
+    for i <- 0 until this.nrows
+        j <- 0 until that.ncolumns
+        k <- 0 until this.ncolumns do
+      result(i, j) = add(result(i, j), combine(this(i, k), that(k, j)))
+    result
+
+  inline def kronecker[O <: Int, P <: Int, B, C : ClassTag](that: Matrix[O, P, B],
+      inline combine: (A, B) => C): Matrix[M*O, N*P, C] =
+    this.flatMap(a => that.map(b => combine(a, b)))
+
+  inline def show: String = rows.map(row => row.mkString(",")).mkString("\n")
+
+  override def equals(that: Any): Boolean = that match
+    case that: Matrix[m, n, _] => (this.data zip that.data).forall(_ == _)
+    case _ => false
+
+extension [M <: Int, N <: Int, O <: Int, P <: Int, A : ClassTag](nested: Matrix[M, N, Matrix[O, P, A]])
+  inline def flatten: Matrix[M*O, N*P, A] =
+    val o = constValue[O]; val p = constValue[P]
+    Matrix.tabulate[M*O, N*P, A]((i, j) => nested(i/o, j/p)(i%o, j%p))
+
+extension [M <: Int, N <: Int, A : ClassTag] (g: Matrix[M, N, A])(using n: Fractional[A])
+  inline def average: A = n.div(g.data.fold(n.zero)(n.plus), n.fromInt(g.data.length))
+
+extension [M <: Int, N <: Int, A : ClassTag] (g: Matrix[M, N, A])(using n: Ordering[A])
+  inline def median: A = g.data.sorted(using n)(g.data.length/2)
+
+
+object Matrix:
+  inline def from[M <: Int, N <: Int, A : ClassTag](as: IterableOnce[A]): Matrix[M, N, A] = Matrix(Array.from(as))
+
+  inline def tabulate[M <: Int, N <: Int, A : ClassTag](inline f: (Int, Int) => A): Matrix[M, N, A] =
+    val m = constValue[M]
+    val n = constValue[N]
+    val data = new Array[A](n*m)
+    var j = 0
+    while j < m do
+      var i = 0
+      while i < n do
+        data(j*n + i) = f(j, i)
+        i += 1
+      j += 1
+    Matrix(data)
+
+  inline def fill[M <: Int, N <: Int, A : ClassTag](v: A): Matrix[M, N, A] = Matrix.tabulate((_, _) => v)
--- collection/src/test/scala/Examples.scala
+++ collection/src/test/scala/Examples.scala
@@ -3,20 +3,20 @@ import munit.FunSuite
 import macroloop.collection.*
 
 class Example extends FunSuite:
-  val g23 = Grid.from[2, 3, Int](Seq(1, 3, 6, 1, 2, 4))
+  val g23 = Matrix.from[2, 3, Int](Seq(1, 3, 6, 1, 2, 4))
 
-  val g22 = Grid.from[2, 2, Int](Seq(4, 10, 16, 20))
+  val g22 = Matrix.from[2, 2, Int](Seq(4, 10, 16, 20))
 
-  val g44 = g22.flatMap(i => Grid.from[2, 2, Int](Seq(i - 3, i - 2, i - 1, i)))
+  val g44 = g22.flatMap(i => Matrix.from[2, 2, Int](Seq(i - 3, i - 2, i - 1, i)))
 
-  val m1 = Grid.from[2, 3, Int](Seq(2, -3, 4, 53, 3, 5))
-  val m2 = Grid.from[3, 2, Int](Seq(3, 3, 5, 0, -3, 4))
-  val m3 = Grid.from[2, 2, Int](Seq(-21, 22, 159, 179))
+  val m1 = Matrix.from[2, 3, Int](Seq(2, -3, 4, 53, 3, 5))
+  val m2 = Matrix.from[3, 2, Int](Seq(3, 3, 5, 0, -3, 4))
+  val m3 = Matrix.from[2, 2, Int](Seq(-21, 22, 159, 179))
 
   test("directional") {
     enum Pos { case TL, TR, BL, BR }
     println(g44.convolve(
-      Grid.from[2, 2, Pos](Pos.values),
+      Matrix.from[2, 2, Pos](Pos.values),
       (i, p) => Seq(p -> i), _ ++ _, Nil
     ).slice[1, 4, 1, 4].show)
   }
@@ -24,13 +24,13 @@ class Example extends FunSuite:
   test("numerical") {
     val g44f = g44.map(_.toFloat)
     println(g44f.convolve(
-      Grid.from[2, 2, Float](Seq(0.25f, 0.25f, 0.25f, 0.25f)),
+      Matrix.from[2, 2, Float](Seq(0.25f, 0.25f, 0.25f, 0.25f)),
       _ * _, _ + _, 0f
     ).show)
   }
 
   test("construct") {
-    println(Grid.tabulate[3, 4, Int]((i, j) => i*10 + j).flatMap(i => Grid.from[1, 2, Int](Seq(i, i*2))).show)
+    println(Matrix.tabulate[3, 4, Int]((i, j) => i*10 + j).flatMap(i => Matrix.from[1, 2, Int](Seq(i, i*2))).show)
   }
 
   test("mm") {
